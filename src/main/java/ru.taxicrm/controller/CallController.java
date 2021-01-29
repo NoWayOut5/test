cec15ed3449a0405cfb95921e62b83f048f01c87
kborodulin <kborodulin@diasoft.ru>
2021-01-29 11:33:13 +0500

    CrossOrigin

--- src/main/java/ru.taxicrm/controller/CallController.java
+++ src/main/java/ru.taxicrm/controller/CallController.java
@@ -1,15 +1,13 @@
 package ru.taxicrm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.taxicrm.domain.Call;
 import ru.taxicrm.service.CallService;
 
 @RestController
 @RequestMapping("call")
+@CrossOrigin
 public class CallController {
 
     private CallService callService;
--- src/main/java/ru.taxicrm/controller/ClientController.java
+++ src/main/java/ru.taxicrm/controller/ClientController.java
@@ -1,15 +1,13 @@
 package ru.taxicrm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.taxicrm.domain.Client;
 import ru.taxicrm.service.ClientService;
 
 @RestController
 @RequestMapping("client")
+@CrossOrigin
 public class ClientController {
 
     private ClientService clientService;
