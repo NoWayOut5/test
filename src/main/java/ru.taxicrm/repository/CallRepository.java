88205ab7ccc98de70fe36cee2ad54276ebf7a674
kborodulin <36662870+kborodulin@users.noreply.github.com>
2021-03-02 12:08:24 +0500

    Delete src directory

--- src/main/java/ru.taxicrm/repository/CallRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.taxicrm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.taxicrm.domain.Call;
-
-@Repository
-public interface CallRepository extends JpaRepository<Call, Long> {
-}
